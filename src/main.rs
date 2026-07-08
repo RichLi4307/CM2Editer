47ef82689be2ee67752c9a33a951f59359ad84a7
RichLi <577955714@qq.com>
2026-07-09 00:52:35 +0800

    fix: dynamic window size = 75% of monitor, min 1024x600

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use CM2Editer::app::App;
 fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "CM2Editer",
-        eframe::NativeOptions {
-            viewport: egui::ViewportBuilder::default()
-                .with_inner_size([1440.0, 900.0]),
-            ..Default::default()
-        },
-        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+        eframe::NativeOptions::default(),
+        Box::new(|cc| {
+            let monitor = cc.egui_ctx.input(|i| i.viewport().monitor_size);
+            if let Some(size) = monitor {
+                let w = (size.x as f32 * 0.75).max(1024.0);
+                let h = (size.y as f32 * 0.75).max(600.0);
+                cc.egui_ctx
+                    .send_viewport_cmd(egui::ViewportCommand::InnerSize(egui::vec2(w, h)));
+            }
+            Ok(Box::new(App::new(cc)))
+        }),
     )
 }
