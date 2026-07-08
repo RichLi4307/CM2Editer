e459999486ce8e0e4f8762525a3b6860e24a16e6
RichLi <577955714@qq.com>
2026-07-09 00:48:59 +0800

    feat: set default window size to 1440x900 on startup

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use CM2Editer::app::App;
 fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "CM2Editer",
-        eframe::NativeOptions::default(),
+        eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default()
+                .with_inner_size([1440.0, 900.0]),
+            ..Default::default()
+        },
         Box::new(|cc| Ok(Box::new(App::new(cc)))),
     )
 }
